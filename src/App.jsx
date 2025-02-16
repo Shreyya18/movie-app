b7f861620ab11cf4c6f8695fc364657e606cbbba
shreyya18 <shreyapoojary251@gmail.com>
2025-02-16 19:20:24 +0530

    update API urls to HTTPS

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
 // fd94b136
-const API_URL = `http://www.omdbapi.com?apikey=${import.meta.env.VITE_OMDB_API_KEY}`;
+const API_URL = `https://www.omdbapi.com?apikey=${import.meta.env.VITE_OMDB_API_KEY}`;
 console.log('API Key:', import.meta.env.VITE_OMDB_API_KEY);
 
 
